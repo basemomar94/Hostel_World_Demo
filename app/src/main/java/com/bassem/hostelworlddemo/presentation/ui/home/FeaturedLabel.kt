791887b91087e971241bc6e78cdb5f2846b7ebb3
Bassem <BasemOmar2012@gmail.com>
2024-11-29 15:57:12 +0200

    remove unused imports

--- app/src/main/java/com/bassem/hostelworlddemo/presentation/ui/home/FeaturedLabel.kt
+++ app/src/main/java/com/bassem/hostelworlddemo/presentation/ui/home/FeaturedLabel.kt
@@ -9,9 +9,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,7 +21,6 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.bassem.hostelworlddemo.R
 import com.bassem.hostelworlddemo.presentation.ui.theme.gold
 
--- app/src/main/java/com/bassem/hostelworlddemo/presentation/ui/home/PropertyItemView.kt
+++ app/src/main/java/com/bassem/hostelworlddemo/presentation/ui/home/PropertyItemView.kt
@@ -1,6 +1,5 @@
 package com.bassem.hostelworlddemo.presentation.ui.home
 
-import android.text.Layout
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,14 +10,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bassem.hostelworlddemo.R

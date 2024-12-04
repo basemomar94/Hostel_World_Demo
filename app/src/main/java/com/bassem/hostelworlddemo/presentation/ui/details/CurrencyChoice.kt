be931724b0efe90a49208dc64387bf62e5635bb3
basemomar94 <basemomar2012@gmail.com>
2024-12-04 19:39:11 +0200

    remove unused imports

--- app/src/main/java/com/bassem/hostelworlddemo/presentation/ui/details/CurrencyChoice.kt
+++ app/src/main/java/com/bassem/hostelworlddemo/presentation/ui/details/CurrencyChoice.kt
@@ -1,8 +1,6 @@
 package com.bassem.hostelworlddemo.presentation.ui.details
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
